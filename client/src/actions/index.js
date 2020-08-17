4e42213f0d1e6bee768509532015e010509dc905
Mohit Bisht <51907464+Mohitb07@users.noreply.github.com>
2020-08-17 21:01:38 +0530

    edit and delete btn, history object for nav

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {
     SIGN_IN,
     SIGN_OUT,
@@ -23,10 +24,12 @@ export const signOut = () =>{
 }
 
 // for CREATING NEW STREAMS
-export const createStream = formValues => async dispatch => {
-    const response = await streams.post('/streams', formValues)
+export const createStream = formValues => async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await streams.post('/streams', {...formValues, userId})
 
     dispatch( { type: CREATE_STREAM, payload: response.data } )
+    history.push('/')
 }
 
 // for FETCHING ALL THE STREAMS
